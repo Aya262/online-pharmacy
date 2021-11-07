46f06c741e5a0fc9fb0fad7788ff40704ec8f395
aya <ayaabdelhaves1317@gmail.com>
2021-11-07 11:02:00 +0200

    add initial items

--- medicines/views.py
+++ medicines/views.py
@@ -2,12 +2,33 @@ from django.shortcuts import render
 from .models import *
 # Create your views here.
 def home(request):
-    products_tablets=Products.objects.filter(pCategories='tablets')[:4].values()
-    products_injection=Products.objects.filter(pCategories='injection')[:4].values()
-    products_drink=Products.objects.filter(pCategories='drink')[:4].values()
+    products_tablets_0=Products.objects.filter(pCategories='tablets')[0]
+    products_tablets_1 = Products.objects.filter(pCategories='tablets')[1]
+    products_tablets_2 = Products.objects.filter(pCategories='tablets')[2]
+    products_tablets_3 = Products.objects.filter(pCategories='tablets')[3]
+
+    products_injection_0=Products.objects.filter(pCategories='injection')[0]
+    products_injection_1= Products.objects.filter(pCategories='injection')[1]
+    products_injection_2 = Products.objects.filter(pCategories='injection')[2]
+    products_injection_3= Products.objects.filter(pCategories='injection')[3]
+
+    products_drink_0=Products.objects.filter(pCategories='drink')[0]
+    products_drink_1 = Products.objects.filter(pCategories='drink')[1]
+    products_drink_2 = Products.objects.filter(pCategories='drink')[2]
+    products_drink_3= Products.objects.filter(pCategories='drink')[3]
+
     context={
-    'products_tablets':products_tablets,
-        'products_injection':products_injection,
-        'products_drink':products_drink
+    'products_tablets_0':products_tablets_0,
+        'products_tablets_1':products_tablets_1,
+        'products_tablets_2':products_tablets_2,
+        'products_tablets_3':products_tablets_3,
+        'products_injection_0':products_injection_0,
+        'products_injection_1':products_injection_1,
+        'products_injection_2':products_injection_2,
+        'products_injection_3':products_injection_3,
+        'products_drink_0':products_drink_0,
+        'products_drink_1':products_drink_1,
+        'products_drink_2':products_drink_2,
+        'products_drink_3':products_drink_3
     }
     return render(request,'medicines/home.html',context)
