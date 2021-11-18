1b49dc15d39df42915f9e1358e69b0c6d47b0b89
aya <ayaabdelhaves1317@gmail.com>
2021-11-18 15:56:02 +0200

    updates

--- medicines/views.py
+++ medicines/views.py
@@ -2,35 +2,16 @@ from django.shortcuts import render
 from .models import *
 # Create your views here.
 def home(request):
-    products_tablets_0=Products.objects.filter(pCategories='tablets')[0]
-    products_tablets_1 = Products.objects.filter(pCategories='tablets')[1]
-    products_tablets_2 = Products.objects.filter(pCategories='tablets')[2]
-    products_tablets_3 = Products.objects.filter(pCategories='tablets')[3]
+    products_drink=Products.objects.filter(pCategories='drink')
+    products_drink=dict(enumerate(products_drink))
+    products_tablets = Products.objects.filter(pCategories='tablets')
+    products_tablets = dict(enumerate(products_tablets))
+    products_injection = Products.objects.filter(pCategories='injection')
+    products_injection = dict(enumerate(products_injection))
 
-    products_injection_0=Products.objects.filter(pCategories='injection')[0]
-    products_injection_1= Products.objects.filter(pCategories='injection')[1]
-    products_injection_2 = Products.objects.filter(pCategories='injection')[2]
-    products_injection_3= Products.objects.filter(pCategories='injection')[3]
-
-    products_drink_0=Products.objects.filter(pCategories='drink')[0]
-    products_drink_1 = Products.objects.filter(pCategories='drink')[1]
-    products_drink_2 = Products.objects.filter(pCategories='drink')[2]
-    products_drink_3= Products.objects.filter(pCategories='drink')[3]
-
-    context={
-    'products_tablets_0':products_tablets_0,
-        'products_tablets_1':products_tablets_1,
-        'products_tablets_2':products_tablets_2,
-        'products_tablets_3':products_tablets_3,
-        'products_injection_0':products_injection_0,
-        'products_injection_1':products_injection_1,
-        'products_injection_2':products_injection_2,
-        'products_injection_3':products_injection_3,
-        'products_drink_0':products_drink_0,
-        'products_drink_1':products_drink_1,
-        'products_drink_2':products_drink_2,
-        'products_drink_3':products_drink_3
-    }
+    context={'products_drink':products_drink,
+             'products_tablets':products_tablets,
+             'products_injection':products_injection}
     return render(request,'medicines/home.html',context)
 
 def register(request):
